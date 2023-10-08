0458e0e29bfd59cfabb391746f041f1d25e262eb
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-10-08 17:36:49 +0700

    arrow f 2

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
 const print = (input) => `${input}`;
+const sum = (numbers) => numbers.reduce((acc, cur) => acc + cur, 0);
 
-console.log(print('Hello World'));
+// console.log(print('Hello World'));
+console.log(sum([1, 2, 3, 4, 5]));
