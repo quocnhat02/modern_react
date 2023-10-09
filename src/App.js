62790e697bb265afa25466b4538fdbbe6ae79e09
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-10-10 00:15:59 +0700

    hello

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const title = 'React';
+  return (
+    <div>
+      <h1>Hello World {title}</h1>
+    </div>
+  );
+}
+
+export default App;
