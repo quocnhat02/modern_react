483cd97933de69114f481637de61da2e7fdb4cc2
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-10-10 00:42:02 +0700

    instanting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
+const List = () => <div>List</div>;
 function App() {
   const title = 'React';
   return (
     <div>
       <h1>Hello World {title}</h1>
+      <List />
     </div>
   );
 }
