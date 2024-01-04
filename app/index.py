bca8ac97cf05529d7f5f40e9dda31ea5f9d89e25
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2024-01-04 15:21:37 +0700

    read file

--- app/index.py
+++ app/index.py
@@ -1,9 +1,5 @@
-def factorial(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n * factorial(n - 1)
-    
-result = factorial(5)
+file = open("./app/text.txt", 'r')
 
-print(result)   
+print(file.read())
+
+file.close()
