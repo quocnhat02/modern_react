6e6bc70cf1f261c1003d6719f8fff560a4d829f0
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-04-15 16:36:35 +0700

    service 3

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -47,7 +47,7 @@ export class UsersService {
     email: string;
     role: 'INTERN' | 'ENGINEER' | 'ADMIN';
   }) {
-    const usersByHighestId = [...this.users].sort((a, b) => (b.id = a.id));
+    const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id);
     const newUser = {
       id: usersByHighestId[0].id + 1,
       ...user,
