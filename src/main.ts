353b763ca1db6ce336a196cbfe336cc364d1bbe2
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-05-25 10:41:33 +0700

    module and run app

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Module } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 
 @Controller()
 class AppController {
@@ -7,3 +8,19 @@ class AppController {
     return 'hello world.';
   }
 }
+
+@Module({
+  controllers: [AppController],
+})
+class AppModule {}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const PORT = 3456;
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+bootstrap();
