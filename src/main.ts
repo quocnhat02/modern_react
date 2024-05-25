3bcdbbd7d9e28f8cb3a85c61b0993daf9701b0d9
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-05-25 10:57:40 +0700

    generate module cli

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { MessagesModule } from './messages/messages.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const PORT = 3456;
-
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+  const app = await NestFactory.create(MessagesModule);
+  await app.listen(3000);
 }
-
 bootstrap();
