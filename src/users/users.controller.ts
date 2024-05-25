628b5c3f3a17c8af570fb00c83b79d2c5a31b481
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-05-25 19:49:23 +0700

    decorator controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,7 +8,6 @@ import {
   Patch,
   Post,
   Query,
-  UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -17,6 +16,7 @@ import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
@@ -25,8 +25,6 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
-  //   @UseInterceptors(new SerializeInterceptor(UserDto))
-  @Serialize(UserDto)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
