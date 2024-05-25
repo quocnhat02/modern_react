8b5a502b9b023d71e9972a4aa5ad841d05b92dbd
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-05-25 14:42:42 +0700

    setup app

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { MessagesModule } from './messages/messages.module';
-import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(MessagesModule);
-  app.useGlobalPipes(new ValidationPipe());
+  const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
 bootstrap();
