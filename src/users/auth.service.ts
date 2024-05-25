00d1ab4ef1e8cd0daa5d6147052dc145c80ff23f
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-05-25 21:30:45 +0700

    auth service 3

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,5 +1,9 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
@@ -10,6 +14,10 @@ export class AuthService {
     if (users.length) {
       throw new BadRequestException('email in use');
     }
+
+    const salt = randomBytes(8).toString('hex');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    const result = salt + '.' + hash.toString('hex');
   }
 
   signIn() {}
